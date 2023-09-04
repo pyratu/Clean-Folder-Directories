e4a2cafaf9dd972e6225ad5eeab87d135c6fb984
pyratu <paul_pyratu@yahoo.com>
2023-09-05 00:45:53 +0300

    first commit

---
+++ DesktopClean/Program.cs
@@ -0,0 +1,41 @@
+﻿string path = @"C:\Users\paul_\Desktop";
+
+string[] files = Directory.GetFiles(path);
+
+List<string> fileExtension = new List<string>();
+
+//takes every file name from desktop files, replaces the prefix(c/users/etc to ""), creates folders with file extension name
+foreach (string file in files)
+{
+
+    string str = file.Replace(path+@"\", "");
+    string wordToAdd = string.Empty;
+    if(str.Length > 3)
+    {
+        wordToAdd = $"{str[str.Length - 3]}{str[str.Length - 2]}{str[str.Length - 1]}";
+        wordToAdd = wordToAdd.Replace(".", "");
+        fileExtension.Add(wordToAdd);
+    }
+    else
+    {
+        wordToAdd = str;
+        wordToAdd = wordToAdd.Replace(".", "");
+        fileExtension.Add(wordToAdd);
+    }
+
+    //creates directory to move files, check if file exist if not -> copy file to dir
+    string dir = @$"C:\New folder\{fileExtension.Last()}_Files";
+    
+    if (!Directory.Exists(dir))
+    {
+        Directory.CreateDirectory(dir);
+    }
+    if(!File.Exists(dir + "\\" + str))
+        File.Copy(file, dir +"\\"+str);
+}
+
+
+fileExtension.Sort();
+
+foreach (var file in fileExtension)
+    Console.WriteLine(file);
